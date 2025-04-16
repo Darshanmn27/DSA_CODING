061e660e682505339009acf4eea0cf06877e7213
darshan <153371797+Darshanmn27@users.noreply.github.com>
2025-04-16 06:20:22 +0530

    Update Fibanocci_series_Dynamic.java

--- Day_7_Fibanocci/Fibanocci_series_Dynamic.java
+++ Day_7_Fibanocci/Fibanocci_series_Dynamic.java
@@ -4,8 +4,18 @@ public class Fibanocci_series_Dynamic
     public static void fibanocci_dynamic()
     {
         System.out.println(" dynamic programming concepts :");
-        int N = 5;
+        int N = 0;
+        if(N<=0)
+        {
+            return ;
+        }
         int[] fib = new int[N];
+        System.out.println("integer array default  values");
+        for(int num:fib)
+        {
+            System.out.print(num+" ");
+        }
+        System.out.println();
 
         fib[0] = 0;
         fib[1] = 1;
@@ -17,7 +27,7 @@ public class Fibanocci_series_Dynamic
 
         for (int i = 0; i < N; i++) 
         {
-            System.out.print(fib[i] + ", ");
+            System.out.print(fib[i] + " ");
         }
     }
 
